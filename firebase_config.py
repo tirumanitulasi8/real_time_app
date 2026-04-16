fca6ed7d0eab7250f566b249ab3ec794b67552ef
Tulasi Tirumani <tirumanitulasi8.com>
2026-04-16 09:15:25 +0530

    use local firebase key

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,7 @@
-import os
-import json
 import firebase_admin
 from firebase_admin import credentials, db
 
-firebase_key = json.loads(os.environ.get("FIREBASE_KEY"))
-
-cred = credentials.Certificate(firebase_key)
+cred = credentials.Certificate("serviceAccountKey.json")
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://real-time-app-f9712-default-rtdb.firebaseio.com/'
