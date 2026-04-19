fe2ae4a67c6070cb570b663b78b7f806fbff03a7
Tulasi Tirumani <tirumanitulasi8.com>
2026-04-19 19:09:11 +0530

    final firebase config update

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, db
 import os
+import json
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+firebase_key = os.environ.get("FIREBASE_KEY")
 
-cred = credentials.Certificate(os.path.join(BASE_DIR, "serviceAccountKey.json"))
+if firebase_key:
+    cred = credentials.Certificate(json.loads(firebase_key))
+else:
+    cred = credentials.Certificate("serviceAccountKey.json")
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://real-time-app-f9712-default-rtdb.firebaseio.com/'
